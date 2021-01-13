c7a37b0cde823818046da21820ddc8f5e6950e6b
Michael Wegelin <mw@michaelwegelin.com>
2021-01-13 21:46:53 +0100

    remote

--- HelloGit/src/de/wegelin/git/MainApp.java
+++ HelloGit/src/de/wegelin/git/MainApp.java
@@ -3,7 +3,7 @@ package de.wegelin.git;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git, you rock!");
+		System.out.println("Hello git, you rock even remotely!");
 	}
 
 }
