c0c62b4d4d2ccf12cc2e8b1d117e15ac341a7209
Michael Wegelin <mw@michaelwegelin.com>
2021-01-13 21:26:43 +0100

    Change message

--- HelloGit/src/de/wegelin/git/MainApp.java
+++ HelloGit/src/de/wegelin/git/MainApp.java
@@ -3,7 +3,7 @@ package de.wegelin.git;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git!");
+		System.out.println("Hello git, you rock!");
 	}
 
 }
